b83252bff109973082a9384b128fc4a802f939cd
manopinkzz <mano.pinkzz@gmail.com>
2017-10-07 21:04:59 +0530

    Test Addition

---
+++ src/main/java/com/microstore/application/mssqlConnect.java
@@ -0,0 +1,99 @@
+package com.microstore.application;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class mssqlConnect {
+	
+	static String className = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
+	static String connectionString ="jdbc:sqlserver://localhost:1433;DatabaseName=CODMWF";
+	static String user = "sa";
+	static String password = "Password123";
+	
+	public static List<String> fetchDropdownValues(String sql) throws SQLException
+	{
+		List<String> dropDownValues = new ArrayList<String>();
+		try {
+			Class.forName(className);
+			Connection con = DriverManager.getConnection(connectionString,user,password);
+			Statement stmt = con.createStatement();
+			ResultSet res = stmt.executeQuery(sql);
+			while(res.next()) {
+				dropDownValues.add(res.getString(1));
+			}
+			
+			return dropDownValues;
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			dropDownValues.add(e.toString());
+			return dropDownValues;
+		}
+	}
+	
+	public static String fetchTableValues(String columns, String sql) throws SQLException, ClassNotFoundException
+	{
+		String tableData = "<thead>"+
+						   "<tr>";
+		String[] abc = columns.split(",");
+		for(String a:abc)
+			tableData = tableData + "<th>" + a + "</th>";
+		tableData = tableData + "</tr>"+
+								"</thead>"+ 
+								"<tbody>";
+		Class.forName(className);
+		Connection con = DriverManager.getConnection(connectionString,user,password);
+		Statement stmt = con.createStatement();
+		ResultSet res = stmt.executeQuery(sql);
+		int colCount= res.getMetaData().getColumnCount();
+		
+		while(res.next()) {
+			tableData = tableData + "<tr>";
+			for(int i=1;i<=colCount;i++)
+				tableData = tableData + "<td>" + res.getString(i) + "</td>";
+			tableData = tableData + "</tr>";
+		}
+		tableData = tableData + "</tbody>";
+		return tableData;
+	}
+	
+	public static String verifyAndAddValues(String verifyQuery, String dataAddQuery) throws SQLException, ClassNotFoundException
+	{
+		Class.forName(className);
+		Connection con = DriverManager.getConnection(connectionString,user,password);
+		Statement stmt = con.createStatement();
+		ResultSet res = stmt.executeQuery(verifyQuery);
+		String sval = "Data Doesn't Exist";
+		while(res.next()) {
+			if(res.getString(1).equals(""))
+			{
+				sval="success";
+				break;
+			}
+			else
+			{
+				sval="already exists";
+				break;
+			}
+		}
+		if(sval.equals("Data Doesn't Exist"))
+		{
+			try {
+				stmt.executeUpdate(dataAddQuery);
+				sval="success";
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				sval="Error : "+e.toString();
+			}
+		}
+		return sval;
+	}
+
+	
+}
---
+++ src/main/webapp/WEB-INF/views/microstore_ui/js/globalvariable.js
@@ -0,0 +1,8 @@
+//var localUrl = "http://localhost:8080/jumbo/";
+//var globalUrl = "http://localhost:8080/jumbo/";
+
+/*****My System Office*****/
+var localUrl = "http://localhost:8080/application/";
+var globalUrl = "http://localhost:8080/application/";
+
+
